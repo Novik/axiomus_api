5cc4c35ef0b6aa2db64b6c9cb8e949ae0cdf8e69
Novik <novik65@gmail.com>
2017-01-30 10:59:06 +0300

    minor fix

--- lib/axiomus_api/delivset.rb
+++ lib/axiomus_api/delivset.rb
@@ -2,7 +2,7 @@ require_relative 'base'
 require_relative 'below'
 
 class AxiomusApi::Delivset < AxiomusApi::Base
-  xml_attribute :return_price, :above_sum, :above_price
+  xml_attribute :return_price, :above_price
   xml_field_array :below, type: AxiomusApi::Below, max_occurs: 3, optional: true
 
   def add_below_element
