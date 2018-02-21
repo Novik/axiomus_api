9f2773019165c689aec1be1963534d00ab5b1f89
Novik <novik65@gmail.com>
2018-02-21 15:27:59 +0300

    work in progress

--- lib/axiomus_api/services/boxberry_services.rb
+++ lib/axiomus_api/services/boxberry_services.rb
@@ -2,6 +2,6 @@ require_relative '../base'
 
 class AxiomusApi::BoxberryServices < AxiomusApi::Base
 
-  xml_attribute :cod, :checkup, :part_return, :card, :cheque, optional: true
+  xml_attribute :cod, :checkup, :part_return, optional: true
 
 end
